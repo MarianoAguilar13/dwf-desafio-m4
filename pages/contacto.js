4d65102ae01c6befee2980c60569672139b49dc4
Mariano <mariano_aguilar_13@hotmail.com>
2022-03-14 17:18:20 -0300

    sexto commit - agregando la funcionalidad del form a contacto

--- pages/contacto.js
+++ pages/contacto.js
@@ -23,6 +23,32 @@ function main() {
   botonCerrarMenuEl.addEventListener("click", () => {
     menuEl.style.display = "";
   });
+
+  const formEl = document.querySelector(".contenedor-form__form");
+
+  formEl.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    const formData = new FormData(evento.target);
+    const valores = Object.fromEntries(formData.entries());
+    valores["most-diff-subject"] = formData.getAll("most-diff-subject");
+
+    fetch("https://apx-api.vercel.app/api/utils/dwf", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        to: "marianocanya@gmail.com",
+        message:
+          "nombre: " +
+          valores.nombre +
+          "/ email: " +
+          valores.email +
+          "/ su mensaje es: " +
+          valores.msj,
+      }),
+    }).catch((error) => {
+      console.log(error);
+    });
+  });
 }
 
 main();
